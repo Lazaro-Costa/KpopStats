4615a26a9bd6482bbbdcf62c82b4212a42abd1b2
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-10-27 09:16:29 -0300

    Created card component fully

--- src/Components/Card/CardFront/CardFrontComponent.tsx
+++ src/Components/Card/CardFront/CardFrontComponent.tsx
@@ -1,25 +1,25 @@
-import { CardFront } from '.'
-import PhotoItem from '../../Photo/PhotoItem'
-import { Label } from '../../Label'
-import { IDataIdol } from '../../../Interfaces/DataIdol'
+import { CardFront } from '.';
+import PhotoItem from '../../Photo/PhotoItem';
+import { Label } from '../../Label';
+import { IDataIdol } from '../../../Interfaces/DataIdol';
 
-const CardComponent = ({data}: {data: IDataIdol}) => {
+const CardComponent = ({ data }: { data: IDataIdol }) => {
   return (
-    <CardFront.Container>
-      <PhotoItem url={data.url} alt={data.group}/>
-      <Label.Root>
-        <div className='grid-rows-2'>
-          <span className='row-span-1 flex gap-1 w-max h-max'>
-            <Label.Small text={data.empresa}/>
-            <Label.Small text={data.fandom}/>
-          </span>
-          <span className='row-span-2'>
-            <Label.Big text={data.group}/>
-          </span>
-        </div>
-      </Label.Root>
-    </CardFront.Container>
-  )
-}
+      <CardFront.Container>
+        <PhotoItem url={data.url} alt={data.group} />
+        <Label.Root>
+          <div className="grid-rows-2">
+            <span className="row-span-1 flex gap-1 w-max h-max">
+              <Label.Small text={data.empresa} />
+              <Label.Small text={data.fandom} />
+            </span>
+            <span className="row-span-2">
+              <Label.Big text={data.group} />
+            </span>
+          </div>
+        </Label.Root>
+      </CardFront.Container>
+  );
+};
 
-export default CardComponent
+export default CardComponent;
