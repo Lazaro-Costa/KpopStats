2dfc720f63ce59b61ca18caac5017f9508df99a9
Lazaro-Costa <lazarocosta.dev@gmail.com>
2024-01-02 11:15:06 -0300

    Refactored Login

--- src/Components/Cadastro/Login/Login.tsx
+++ src/Components/Cadastro/Login/Login.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { apiBase } from '../../Helper/Variables';
 import Head from '../../Helper/Head';
 import { useUserStore } from '../../../UserZustand';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../../Loading/Loading';
+import { trim } from '../../../utils/trim';
+import style from './CSS/LoginForm.module.css';
 
 const Login = () => {
-  const { user, login, loading} = useUserStore();
+  const { user, login, loading } = useUserStore();
   const [form, setForm] = React.useState({
     username: '',
     password: '',
@@ -14,80 +15,61 @@ const Login = () => {
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    if(user?.username){
+    if (user?.username) {
       navigate('/');
     }
-  }, [user])
+  }, [user]);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      await login(trim(form.username), trim(form.password));
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login(form.username, form.password);
-    console.log("HandleSubmit Login " + user?.username);
-  };
-  const handleTest = e => {
-    e.preventDefault();
-    fetch(`${apiBase}/users/logout`, {
-      method: 'POST',
-      credentials: 'include',
-    })
-      .then(response => response.json())
-      .then(user => console.log(user));
-  };
   return (
     <React.Fragment>
       <Head title="Login" description="Login area" />
       {loading && !user ? (
         <Loading />
       ) : (
-          <div className="text-slate-200 flex p-2 gap-4 bg-zinc-800 animeLeft">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-8">
-              <div className="flex flex-col w-48">
-                <label htmlFor="username">Username</label>
-                <pre>{JSON.stringify({'username': 'MidzyTest2'})}</pre>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  className="text-zinc-800"
-                  onChange={e => setForm({ ...form, username: e.target.value })}
-                />
-              </div>
-
-              <div className="flex flex-col w-48">
-                <label htmlFor="password">Password</label>
-                <pre>{JSON.stringify({'password': 'UIFeets2'})}</pre>
-
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="text-zinc-800"
-                  onChange={e => setForm({ ...form, password: e.target.value })}
-                />
-              </div>
-              <button className="text-slate-200 p-2 border border-red-500 w-max">
-                Login
-              </button>
-            </form>
-            <pre className="text-slate-200">
-              {user && JSON.stringify(user, null, 2)}
-            </pre>
-            <div className="flex flex-col border border-slate-200 rounded-sm p-2">
-              <h1 className="text-slate-200">Logout</h1>
-
-              <button
-                className="text-slate-200 p-2 border border-red-500 w-max"
-                onClick={e => handleTest(e)}
-              >
-                Testar
-              </button>
-            </div>
+        <div className="text-slate-200 flex p-2 gap-4 justify-center items-center animeLeft self-center">
+          <div className={`${style.login} ${style.wrap}`}>
+          
+            <div className={style.h1}>Login</div>
+            <input
+              placeholder="Username"
+              id="username"
+              name="username"
+              type="text"
+              value={form.username}
+              onChange={e => setForm({ ...form, username: e.target.value })}
+            />
+            <input
+              placeholder="Password"
+              id="password"
+              name="password"
+              type="password"
+              value={form.password}
+              onChange={e => setForm({ ...form, password: e.target.value })}
+            />
+            <button
+              className={style.btn}
+              type="submit"
+              onClick={e => handleSubmit(e)}
+            >Login</button>
           </div>
-        )}
+
+          <pre className="text-slate-200">
+            {user && JSON.stringify(user, null, 2)}
+          </pre>
+        </div>
+      )}
     </React.Fragment>
   );
 };
-//Checar porque o nome não aparece quando o usuário está logado.
+// Continuar checagem do Login. Testar login e logout e melhorar o desing
 
 export default Login;
